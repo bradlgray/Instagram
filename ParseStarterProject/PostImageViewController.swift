44bcdb2cedc96be3e8100adb8777913a3e8b75f5
Bradford Gray <bgray@westmont.edu>
2015-08-17 20:58:40 -0700

    Select and add picture, and upload

--- ParseStarterProject/PostImageViewController.swift
+++ ParseStarterProject/PostImageViewController.swift
@@ -7,9 +7,63 @@
 //
 
 import UIKit
+import Parse
 
-class PostImageViewController: UIViewController {
+class PostImageViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    
+    @IBOutlet weak var ImageToPost: UIImageView!
+    
+    @IBAction func ChooseImage(sender: UIButton) {
+    
+        
+        var image = UIImagePickerController()
+        image.delegate = self
+        image.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        image.allowsEditing = false
+        
+        self.presentViewController(image, animated: true, completion: nil)
+    
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+        
+        ImageToPost.image = image
+        
+    }
+    
+    @IBOutlet weak var imageText: UITextField!
+    
+    
+    @IBAction func postImage(sender: UIButton) {
+        
+       
+        var post = PFObject(className: "Post")
+        
+        post["message"] = imageText.text
+        
+        post["userId"] = PFUser.currentUser()!.objectId!
+        
+        let imageData = UIImagePNGRepresentation(ImageToPost.image!)
+        
+        let imageFile = PFFile(name: "image.png", data: imageData!)
+        
+        post["imageFile"] = imageFile
+        
+        post.saveInBackgroundWithBlock{(success, error) -> Void in
+    
+            
+            if error == nil {
+                
+                print("Sucess")
+                
+            }
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
